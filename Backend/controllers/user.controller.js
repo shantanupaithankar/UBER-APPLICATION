d6527baa74006b9ca55e5f3e881fbffc18a0a785
shantanu paithankar <shantanupaithankar12@gmail.com>
2025-03-21 17:30:24 +0530

    added middleware

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,17 +1,18 @@
 const userModel = require('../models/user.model');
 const userService = require('../services/user.service');
-const {validationResult} = require('express-validator');
+const { validationResult } = require('express-validator');
+const blacklistTokenModel = require('../models/blacklistToken.model');
 
 
 module.exports.registerUser = async (req, res, next) => {
 
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array()});
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
 
     const { fullname, email, password } = req.body;
-    
+
     const hashPassword = await userModel.schema.statics.hashPassword(password);
 
 
@@ -25,31 +26,45 @@ module.exports.registerUser = async (req, res, next) => {
 
     const token = user.generateAuthToken();
 
-    res.status(201).json({token,user});
+    res.status(201).json({ token, user });
 
 }
 
 module.exports.loginUser = async (req, res, next) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array()});
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
 
     const { email, password } = req.body;
 
     const user = await userModel.findOne({ email }).select('+password');
 
-    if(!user){
-        return res.status(401).json({message: 'Invalid email or password'});
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid email or password' });
     }
 
     const isMatch = await user.comparePassword(password);
 
-    if(!isMatch){
-        return res.status(401).json({message: 'Invalid email or password'});
+    if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid email or password' });
     }
 
     const token = user.generateAuthToken();
-    
-    res.status(200).json({token,user});
+
+    res.cookie('token', token);
+
+    res.status(200).json({ token, user });
+}
+
+module.exports.getUserProfile = async (req, res, next) => {
+    res.status(200).json(req.user);
+}
+
+module.exports.logoutUser = async (req, res, next) => {
+    res.clearCookie('token');
+    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+
+    await blacklistTokenModel.create({ token });
+    res.status(200).json({ message: 'Logged out successfully' });
 }
---
+++ Backend/models/blacklistToken.model.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const blacklistTokenSchema = new mongoose.Schema({
+    token: { type: String, required: true, unqiue: true },
+    createdAt: { type: Date, default: Date.now, expires: 86400 } // 86400 seconds = 24 hours
+});
+
+module.exports = mongoose.model('BlacklistToken', blacklistTokenSchema);
